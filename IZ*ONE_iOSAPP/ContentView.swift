5ddc27ff9e2d2cfdf921a0014912c97b05d33097
weilun-hong <eric0912e@gmail.com>
2021-10-31 17:19:18 +0800

    Initial Commit

---
+++ IZ*ONE_iOSAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IZ*ONE_iOSAPP
+//
+//  Created by 洪偉倫 on 2021/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IZ*ONE_iOSAPP/IZ_ONE_iOSAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  IZ_ONE_iOSAPPApp.swift
+//  IZ*ONE_iOSAPP
+//
+//  Created by 洪偉倫 on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct IZ_ONE_iOSAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
